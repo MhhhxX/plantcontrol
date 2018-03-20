3acc94806f925fc55f49da354252034195618b63
MhhhxX <max.christoph@web.de>
2018-03-20 15:33:02 +0100

    chart is now more responsive

--- static/js/resize_canvas.js
+++ static/js/resize_canvas.js
@@ -1,12 +1,32 @@
+var mql = window.matchMedia("(orientation: portrait)").matches;
+
 function resize(){
     var chart = $("#chart");
-    $(chart).attr('height', 0.4 * $(window).height());
-    $(chart).attr('width', $(chart).parent().outerWidth());
-    console.log($(window).height());
+    var chartContainer = $(chart).parent();
+    var windowHeight = Math.floor($(window).height());
+
+    if ($(window).width() <= 767.98)
+        $(chartContainer).css('height', 0.15 * windowHeight + "px");
+    else
+        $(chartContainer).css('height', 0.25 * windowHeight + "px");
 }
+
 $(document).ready(function(){
     resize();
-    $(window).on("resize", function(){
-        resize();
-    });
+
+        $(window).on("resize", function () {
+            if ($(window).width() <= 767.98) {
+                var newMql = window.matchMedia("(orientation: portrait)").matches;
+                if (mql && !newMql) {
+                    mql = newMql;
+                    resize();
+                } else if (!mql && newMql) {
+                    mql = newMql;
+                    resize();
+                }
+            } else {
+                resize();
+            }
+        });
+
 });
