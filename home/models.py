ec6ae4feb9a3b8a3e9a1971b1d37bfdf04b786a6
MhhhxX <max.christoph@web.de>
2018-03-21 18:50:09 +0100

    code refactor

--- home/models.py
+++ home/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your models here.
-
+device_names = {'fan': 'Lüfter', 'light': 'Licht', 'pump': 'Pumpe'}
 
 class RelaisSettings(models.Model):
     relais_id = models.IntegerField(primary_key=True)
@@ -9,6 +10,23 @@ class RelaisSettings(models.Model):
     description = models.CharField(max_length=30, default="Description")
     name = models.CharField(max_length=15, default="Name")
     state = models.BooleanField(default=False)
+
+    @staticmethod
+    def pin_checkup(*pins):
+        results = {}
+        for pin in pins:
+            if pin in device_names:
+                try:
+                    result = RelaisSettings.objects.get(name=device_names[pin]).GPIO_pin
+                    results.update({pin: result})
+                except ObjectDoesNotExist:
+                    print("{} is not specified. Please add a table entry for {} where name is '{}'".
+                          format(pin, pin, device_names[pin]))
+                    return {}
+            else:
+                print("There is no task for {}!".format(pin))
+                return {}
+        return results
 
 
 class SensorSettings(models.Model):
